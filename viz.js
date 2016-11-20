6f3c026a689490a141ef72aac1e531bd77ac9cd2
Chris Roth <chris@cjroth.com>
2016-11-19 22:08:12 -0500

    tabs

--- viz.js
+++ viz.js
@@ -3,27 +3,27 @@ function main(globals) {
     const d3 = globals.d3
     const _ = globals._
 
-	let colors = d3.schemeCategory20
+    let colors = d3.schemeCategory20
 
-	let scene = d3.select('a-scene')
+    let scene = d3.select('a-scene')
 
-	let width = scene.attr('width')
-	let height = scene.attr('height')
+    let width = scene.attr('width')
+    let height = scene.attr('height')
 
     let data = generateData(10, 10, 10, 20)
 
-	let boxes = scene
-	    .selectAll('a-box')
-	    .filter('.box')
-	    .data(data)
-	    .enter()
-		.append('a-box')
-		    .attr('class', 'box')
-			.attr('color', d => colors[d.value])
-			.attr('depth', 1)
-			.attr('height', d => d.y)
-			.attr('width', 1)
-			.attr('position', d => `${d.x} ${d.y / 2} ${d.z}`)
+    let boxes = scene
+        .selectAll('a-box')
+        .filter('.box')
+        .data(data)
+        .enter()
+        .append('a-box')
+        .attr('class', 'box')
+        .attr('color', d => colors[d.value])
+        .attr('depth', 1)
+        .attr('height', d => d.y)
+        .attr('width', 1)
+        .attr('position', d => `${d.x} ${d.y / 2} ${d.z}`)
 
     setInterval(function() {
 
